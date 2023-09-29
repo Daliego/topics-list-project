5f4e581e9c2d2db22fc9a49b4d3fdee3f1e3954c
Diego de Sousa Araujo <daliego82@gmail.com>
2023-09-29 10:41:32 -0300

    Feat: added the select option on form

---
+++ src/pages/topicForm/index.tsx
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react"
+import {v4 as uuid} from 'uuid';
+import { Topic } from "../../models/topic";
+import { authors } from "../../database/localData/topicData";
+
+interface topicFormProps {
+    onSubmit: (topic: Topic) => void
+}
+
+export function TopicForm({onSubmit} : topicFormProps) {
+    const topicText = useRef<HTMLInputElement>(null)
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        
+        const text: string = topicText.current!.value
+
+        const topic: Topic = {
+            id: uuid(),
+            name: text,
+            autor: authors[0],
+            tags: selectedTopics[0] === "" ? ["No tags"] : selectedTopics,
+            likes: 0,
+            active: true, 
+            createdAt: new Date(),
+        }
+
+        onSubmit(topic)
+
+        topicText.current!.value = ""
+    }
+
+    const [selectedTopics, setSelectedTopics] = useState([""]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+            const options = [...e.target.selectedOptions];
+
+            const values = options.map(topic => topic.value);
+            setSelectedTopics(values);
+    }
+
+    return (
+        <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: 'column', justifyContent: "center"}}>
+            <label>
+                Choose your topic tag
+            </label>
+            <select
+            multiple={true}
+            value={selectedTopics}
+            onChange={handleChange}
+            >
+                <option value="programming">Programming</option>
+                <option value="programm">Program</option>
+                <option value="study">Study</option>
+                <option value="studying">Studyind</option>
+                <option value="writing">Writing</option>
+            </select>
+            <input type="text" placeholder="Type your topic..." required ref={topicText}></input>
+            <input type="submit" value={"Add"}></input>
+        </form>
+    )
+}
